1aceb0a5046ffccface7aea44c95aae270e3f421
唐文雍 <tammeny@qq.com>
2020-11-22 14:46:19 +0800

    删除文件

--- sql/specification/create_specification.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `specifications` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(60) NOT NULL DEFAULT '',
-  `sort_order` tinyint(3) unsigned NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COMMENT='规格表';
